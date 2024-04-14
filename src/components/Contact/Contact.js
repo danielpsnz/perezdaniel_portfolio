75d2b2d94e744aad310428056983e6adc57e609b
Daniel Pérez <dpdaniperezsanz@gmail.com>
2024-04-14 21:23:35 +0200

    updated contact backend

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,36 +1,51 @@
-import React, { useState } from "react";
+import { useState } from 'react';
+import emailjs from '@emailjs/browser';
+
 import { Container, Row, Col } from "react-bootstrap";
 import contactImg from "../../assets/images/contact-img.svg";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
 import "./Contact.scss";
 
-const FORM_ENDPOINT = "https://public.herotofu.com/v1/3dcc2530-d25b-11ee-a1c1-7755cb567bfd";
-
 const Contact = () => {
-  const [confirmationMessage, setConfirmationMessage] = useState("");
-  const [formData, setFormData] = useState({
-      name: "",
-      email: "",
-      message: "",
-  });
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [stateMessage, setStateMessage] = useState(null);
 
-  const handleChange = (e) => {
-      setFormData({ ...formData, [e.target.id]: e.target.value });
-  };
-
-  const handleSubmit = (e) => {
-      e.preventDefault();
-
-      // Use formData in your form submission logic
-      console.log(formData);
-
-      setConfirmationMessage("¡Thank you! Your message has been sent.");
+  const sendEmail = (e) => {
+    e.persist();
+    e.preventDefault();
+    setIsSubmitting(true);
+    emailjs
+      .sendForm(
+        'service_mwpasdf',
+        'template_bfkgc7o',
+        e.target,
+        '_73rUp0TBGrThRUP5'
+      )
+      .then(
+        (result) => {
+          setStateMessage('Message sent!');
+          setIsSubmitting(false);
+          setTimeout(() => {
+            setStateMessage(null);
+          }, 5000); // hide message after 5 seconds
+        },
+        (error) => {
+          setStateMessage('Something went wrong, please try again later');
+          setIsSubmitting(false);
+          setTimeout(() => {
+            setStateMessage(null);
+          }, 5000); // hide message after 5 seconds
+        }
+      );
+    
+    // Clears the form after sending the email
+    e.target.reset();
   };
 
     return (
-        <section className="contact" id="connect">
-            <Container>
+      <section className="contact" id="connect">
+          <Container>
             <Row className="align-items-center">
                 <Col size={12} md={6}>
                 <TrackVisibility>
@@ -41,38 +56,34 @@ const Contact = () => {
                 </Col>
 
                 <Col size={12} md={6}>
-            <TrackVisibility>
-              {({ isVisible }) =>
-                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                <h2>Do not hesitate to contact with me!</h2>
-                {confirmationMessage && (
-                                        <div className="confirmation-message">
-                                            {confirmationMessage}
-                                        </div>
-                                    )}
-                <form id="contact-form" action={FORM_ENDPOINT} onSubmit={handleSubmit} method="post" accept-charset="UTF-8">
-                  <Row>
-                    <Col size={12} sm={6} className="px-1">
-                        <label for="name">Name:</label>
-                        <input type="text" id="name" value={formData.name} onChange={handleChange} required />
-                    </Col>
-                    <Col size={12} sm={6} className="px-1">
-                        <label for="exampleInputEmail1">Email:</label>
-                        <input type="text" id="email" value={formData.email} onChange={handleChange} required />
-                    </Col>
-                    <Col size={12} className="px-1">
-                        <label for="message">Message:</label>
-                        <input name="_gotcha" type="text" id="message" value={formData.message} onChange={handleChange} required />
-                    </Col>
-                    <button type="submit" value="Download CTA" className="btn btn-primary">Send</button>
-                  </Row>
-                </form>
-              </div>}
-            </TrackVisibility>
-          </Col>
-        </Row>
-      </Container>
-    </section>
+                  <TrackVisibility>
+                    {({ isVisible }) =>
+                      <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                      <h2>Do not hesitate to contact with me!</h2>
+                      <form id="contact-form" onSubmit={sendEmail} accept-charset="UTF-8">
+                        <Row>
+                          <Col size={12} sm={6} className="px-1">
+                              <label for="name">Name:</label>
+                              <input type="text" id="name" name="user_name" />
+                          </Col>
+                          <Col size={12} sm={6} className="px-1">
+                              <label for="exampleInputEmail1">Email:</label>
+                              <input type="email" id="email" name="user_email" />
+                          </Col>
+                          <Col size={12} className="px-1">
+                              <label for="message">Message:</label>
+                              <input name="message" type="text" id="message" />
+                          </Col>
+                          <input type="submit" className="btn btn-primary" value="Send" disabled={isSubmitting} />
+                          {stateMessage && <p>{stateMessage}</p>}
+                        </Row>
+                      </form>
+                    </div>}
+                  </TrackVisibility>
+                </Col>
+              </Row>
+          </Container>
+      </section>
     );
 }
 
