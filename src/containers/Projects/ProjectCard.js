04393665b2a663dfa76a1bd3de182a6b8a85e748
Daniel Pérez <dpdaniperezsanz@gmail.com>
2024-04-14 20:52:40 +0200

    updated css about page

--- src/containers/Projects/ProjectCard.js
+++ src/containers/Projects/ProjectCard.js
@@ -1,14 +1,17 @@
 import React from "react";
+import Link from "react-dom";
 
 function ProjectCard(props) {
   return (
-    <button href={props.repoLink} className="project-card-view" target="_blank">
+    <a href={props.repoLink}>
+    <button className="project-card-view" target="_blank">
       <div className="content-card">
         <div className="card-title">{props.title}</div>
         <div className="card-text">{props.description}</div>
         <div className="languages">{props.languages}</div>
       </div>
     </button>
+    </a>
   );
 }
 export default ProjectCard;
